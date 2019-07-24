fda94f1017fac96c0b90c0756b02987d52e8cdf8
Kristin Barr <kristinbarr@gmail.com>
2019-07-23 23:06:45 -0400

    added router switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
 
 import Movie from './Movies/Movie'
@@ -15,8 +15,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+      <Switch>
         <Route exact path='/' component={MovieList} />
         <Route path='/movies/:id' component={Movie} />
+      </Switch>
     </div>
   )
 };
