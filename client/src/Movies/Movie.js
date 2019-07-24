ec8a405959f468d76f163863a5ad34adcd1d2732
Kristin Barr <kristinbarr@gmail.com>
2019-07-23 20:53:15 -0400

    updated fixes from parent project

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState();
- 
+  const [movie, setMovie] = useState({});
   useEffect(() => {
     const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-    fetchMovie = id => {
       axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -18,11 +16,10 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-    };
-  });
-  
+  },[]);
+
   // Uncomment this only when you have moved on to the stretch goals
-  // saveMovie = () => {
+  // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
