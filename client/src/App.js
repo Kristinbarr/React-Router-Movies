4f72f1a5e26351426d7b80e2b96e2b4f78f0996b
Kristin Barr <kristinbarr@gmail.com>
2020-03-19 01:52:25 -0400

    save button saves movie to nav list

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
-import SavedList from './Movies/SavedList';
+import SavedList from './Movies/SavedList'
 
 import Movie from './Movies/Movie'
 import MovieList from './Movies/MovieList'
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([])
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+    setSavedList([...savedList, movie])
+  }
 
   return (
     <div>
       <SavedList list={savedList} />
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route path='/movies/:id' render={props => (
-          <Movie {...props} addToSavedList={addToSavedList} />
-        )} />
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={props => <Movie {...props} addToSavedList={addToSavedList} />}
+        />
       </Switch>
     </div>
   )
-};
+}
 
-export default App;
+export default App
